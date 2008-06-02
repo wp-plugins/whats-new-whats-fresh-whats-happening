abe25812868a01ebac4e71674f2ad69a42a15875
bochgoch <bochgoch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-06-02 20:12:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/whats-new-whats-fresh-whats-happening/trunk@48859 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,22 +3,22 @@ Contributors: Martin Ford, Bochgoch
 Donate link: http://www.bochgoch.com/?p=wp
 Tags: posts,comments,links,categories
 Requires at least: 2.2 (for widget)
-Tested up to: 2.3
+Tested up to: 2.5.1
 Stable tag: trunk
 
-This plugin provides an overview of activity on a blog in the form of posts and comments. The plugin is widgetised and embeddable in any HTML page.
+This plugin provides an overview of activity on a blog in the form of posts and comments. The plugin is widgetised, available on a Wordpress page and embeddable into any HTML page on any site of your choice.
 
 == Description ==
 
 This plugin provides configurable links to posts and comments from the blog it is installed into. The output from the plugin can either be displayed as
-a widget in the sidebar on your Wordpress 2.2+ blog OR embedded into any HTML page on any PHP powered site.
+a widget in the sidebar on your Wordpress 2.2+ blog, as a page in your Wordpress blog OR embedded into ANY page on ANY PHP powered site.
 
 Full styling and configuration options are available.
 
->>Configure the number of items (posts and comments) and the method of selecting the items (alphabetically, newest, oldest or random) displayed.
->>Latest Posts: Show post title, date and author and an excerpt of the post content.
->>Latest Comments: Show comment date, author and excerpt of the comment.
->>Styling: allow your site’s styles to cascade or define inline styles in your admin options.
+* Configure the number of items (posts and comments) and the method of selecting the items (alphabetically, newest, oldest or random) displayed.
+* Highlight Posts: Show any of - post title, date and author and an excerpt of the post content.
+* Highlight Comments: Show any of - comment date, author and an excerpt of the comment.
+* Styling: allow your site’s styles to cascade or define inline styles in your admin options.
 
 == Installation ==
 
@@ -27,24 +27,20 @@ Full styling and configuration options are available.
 3. Go to the 'Whats New Whats Fresh Whats Happening' options page (Under 'Options' on the main admin menu) and configure!
 4. To use as a widget click the 'Presentation' then 'Widgets' menu options and drag the 'Whats New Whats Fresh Whats Happening' into your sidebar.
 5. To use embedded into another page:
-	1. Check that the page is hosted on a server that runs PHP - Whats New Whats Fresh Whats Happening needs PHP to run.
-	2. Add the following code to the page in the place you want Whats New Whats Fresh Whats Happening to appear:
+	* Check that the page is hosted on a server that runs PHP - Whats New Whats Fresh Whats Happening needs PHP to run.
+	* Add the following code to the page in the place you want Whats New Whats Fresh Whats Happening to appear:
 
-	<?php include $_SERVER['DOCUMENT_ROOT']."/wordpress/wp-content/plugins/whats_new_whats_fresh_whats_happening.1.4/whats_new_whats_fresh_whats_happening_i.php"; ?>
+	&lt;?php include $_SERVER['DOCUMENT_ROOT']."/wordpress/wp-content/plugins/whats_new_whats_fresh_whats_happening/whats_new_whats_fresh_whats_happening_i.php"; ?&gt;
 
 	...this assumes that you have installed Wordpress into the subdirectory 'wordpress' from the root of your site. Amend if necessary.
-	3. That's it!
+	* That's it!
 
 == Frequently Asked Questions ==
 
 = Where can I leave feedback about the Whats New Whats Fresh Whats Happening plugin? =
 Visit www.bochgoch.com
 
-= The first sentence of my post isn't showing! =
-Ensure that there is no HTML markup in the post before the first sentence. HTML confuses Whats New Whats Fresh Whats Happening.
-
-= The first sentence of my post isn't showing! =
-Ensure that there is no HTML markup in the post before the first sentence. HTML confuses Whats New Whats Fresh Whats Happening.
-
 = Troubleshooting when embedded into another page: =
-Ensure that the paths are correct. The plugin assumes that it has been installed into the directory path /wordpress/wp-content/plugins/whats_new_whats_fresh_whats_happening.1.4/ relative to the root page of the site. If that is not the case then you'll need to amend the WNWFWH code.
+Ensure that the paths are correct. 
+The plugin assumes that it has been installed into the directory path /wordpress/wp-content/plugins/whats_new_whats_fresh_whats_happening/ relative to the root page of the site. 
+If that is not the case then you'll need to amend the WNWFWH code.
--- whats_new_whats_fresh_whats_happening_o.php
+++ whats_new_whats_fresh_whats_happening_o.php
@@ -1,28 +1,52 @@
 <?php
-	global $wpdb;	
+if (!function_exists('sentenceSnip')) {
+  function sentenceSnip($fullPost,$wordCap=10)
+  {
+    $strippedPost = strip_tags($fullPost);
+  	$pieces = split("\. ?", $strippedPost);
+    $piecesWord = explode(' ',$pieces[0]); // explode words in first sentence
+  	$wordsInSentence = count($piecesWord);
+    if ($wordsInSentence < 5 or $wordsInSentence > $wordCap) // short sentence so take first X words instead OR long sentence so cap 
+    { 
+      $piecesWord = explode(' ',$strippedPost); 
+      $piecesWord = array_slice($piecesWord, 0, $wordCap); 
+      $postSentence = implode(' ',$piecesWord).'...';  
+    }  
+    else { $postSentence = trim($pieces[0]).'.'; } 
+    return($postSentence);
+  }
+}
+
+	global $wpdb;
+	global $whatsNewFreshHappeningHTML;	
 
   $numPosts = get_option('wnwfwh_numberOfPosts');
   $numComments = get_option('wnwfwh_numberOfComments');
   $orderBy = get_option('wnwfwh_orderBy');
-  $showPostSentence = get_option('wnwfwh_showPostSentence');
-  $showCommentSentence = get_option('wnwfwh_showCommentSentence');
-  $postsHeading = get_option('wnwfwh_postsHeading');
+  $showSentence = get_option('wnwfwh_showSentence');
+  $showXWords = get_option('wnwfwh_showSentenceXwords');
+  $showAuthor = get_option('wnwfwh_showAuthor');
+	$showDate = get_option('wnwfwh_showDate');
+	$postsHeading = get_option('wnwfwh_postsHeading');
   $commentsHeading = get_option('wnwfwh_commentsHeading');
   $excludeCategories = get_option('wnwfwh_excludeCategories');
-  
+	
   $divStyle = get_option('wnwfwh_divStyle');
   $h5Style = get_option('wnwfwh_h5Style');
   $pStyle = get_option('wnwfwh_pStyle');
   $aStyle = get_option('wnwfwh_aStyle');
   $ulStyle = get_option('wnwfwh_ulStyle');
   $liStyle = get_option('wnwfwh_liStyle');
+
+  $writeToFile = get_option('wnwfwh_writeToFile');
   
 	$writePost1 = false; // set true to write to file
 	
   $whatsNewFreshHappeningHTML = $before_widget.$before_title;
   $whatsNewFreshHappeningHTML = '<div id="whatsnewwhatsfreshwhatshappening" style="'.$divStyle.'">'.Chr(10).$after_title;
 
- 	$version = explode(".", get_bloginfo('version'));
+ 	/* POSTS */
+	$version = explode(".", get_bloginfo('version'));
 	if ( $version[0] > 2 or ($version[0] == 2 and $version[1] >= 3) ) // version greater than 2.3
 	{
 	 if (strlen($excludeCategories) > 0) {$excludeCategories = ' AND t2.term_id NOT IN ('.$excludeCategories.')  AND t2.taxonomy="category" ';}
@@ -39,11 +63,16 @@
     $whatsNewFreshHappeningHTML .= '<h5 style="'.$h5Style.'">'.$postsHeading.'</h5>'.Chr(10);
     foreach ($whatsNewFreshHappenings as $whatsNewFreshHappening) {
     	$postSentence = '';
-      if (substr(strtolower($showPostSentence),0,1) == 'y') {
-      		$pieces = split("\. ?", $whatsNewFreshHappening->post_content);
-          if (strlen(strip_tags($pieces[0])) > 3) {	$postSentence = '<br />'.strip_tags($pieces[0]).'.'; }
+      if (substr(strtolower($showSentence),0,1) == 'y' or substr(strtolower($showSentence),0,1) == 'p') {
+					$postSentence = sentenceSnip($whatsNewFreshHappening->post_content,$showXWords);
     	}
-			$postHTML = '<p style="'.$pStyle.'"><a style="'.$aStyle.'" href="'.get_permalink($whatsNewFreshHappening->ID).'">'.$whatsNewFreshHappening->post_title.'</a> by '.$whatsNewFreshHappening->display_name.' on '.date('F jS, Y',strtotime($whatsNewFreshHappening->post_date)).$postSentence.'</p>'.Chr(10); 
+			$postExtraDetail = false;
+			$postHTML = '<p style="'.$pStyle.'"><a style="'.$aStyle.'" href="'.get_permalink($whatsNewFreshHappening->ID).'">'.$whatsNewFreshHappening->post_title.'</a>'; 
+			if (substr(strtolower($showAuthor),0,1) == 'y' or substr(strtolower($showAuthor),0,1) == 'p') { $postHTML .= ' by '.$whatsNewFreshHappening->display_name; $postExtraDetail = true; } 
+			if (substr(strtolower($showDate),0,1) == 'y' or substr(strtolower($showDate),0,1) == 'p') { $postHTML .= ' on '.date('F jS, Y',strtotime($whatsNewFreshHappening->post_date)); $postExtraDetail = true; } 
+			if ($postExtraDetail) { $postSentence = '<br />'.$postSentence; } 
+			$postHTML .= $postSentence.'</p>'.Chr(10);
+			 
 			$whatsNewFreshHappeningHTML .= $postHTML; 
 			//optionally write out the first post to a file - a further method for embedding posts in non-WP pages
       if ($writePost1)
@@ -57,26 +86,36 @@
     }
   }
 	
+ 	/* COMMENTS */
   $orderBy = str_replace('post_date','comment_date',$orderBy);
   $whatsNewFreshHappenings = $wpdb->get_results("SELECT t1.comment_ID, t1.comment_post_ID, t1.comment_date, t1.comment_content, t1.comment_author, t2.post_title FROM $wpdb->comments t1, $wpdb->posts t2 WHERE t1.comment_post_ID = t2.ID AND t1.comment_approved = '1' ORDER BY ".$orderBy." LIMIT ".$numComments);
   if (count($whatsNewFreshHappenings) > 0) 
   {
     $whatsNewFreshHappeningHTML .= '<h5 style="'.$h5Style.'">'.$commentsHeading.'</h5>'.Chr(10);
-    foreach ($whatsNewFreshHappenings as $whatsNewFreshHappening) {
-      $commentSentence = '';
-      if (substr(strtolower($showCommentSentence),0,1) == 'y') {
-      		$pieces = split("\. ?", $whatsNewFreshHappening->comment_content);
-          if (strlen($pieces[0]) > 3) {	$commentSentence = '<br />'.strip_tags($pieces[0]).'.'; }
+    foreach ($whatsNewFreshHappenings as $whatsNewFreshHappening) {    
+			$commentSentence = '';
+      if (substr(strtolower($showSentence),0,1) == 'y' or substr(strtolower($showSentence),0,1) == 'c') {
+					$commentSentence = sentenceSnip($whatsNewFreshHappening->comment_content,$showXWords);
     	}
-    	$whatsNewFreshHappeningHTML .= '<p style="'.$pStyle.'">On <a style="'.$aStyle.'" href="'.get_permalink($whatsNewFreshHappening->comment_post_ID).'">'.$whatsNewFreshHappening->post_title.' by '.$whatsNewFreshHappening->comment_author.'</a> on '.date('F jS, Y',strtotime($whatsNewFreshHappening->comment_date)).$commentSentence.'</p>'.Chr(10);
+			$commentExtraDetail = false;
+    	$commentHTML = '<p style="'.$pStyle.'"><a style="'.$aStyle.'" href="'.get_permalink($whatsNewFreshHappening->comment_post_ID).'">On '.$whatsNewFreshHappening->post_title.'</a>';
+			if (substr(strtolower($showAuthor),0,1) == 'y' or substr(strtolower($showAuthor),0,1) == 'c') { $commentHTML .= ' by '.$whatsNewFreshHappening->comment_author; $commentExtraDetail = true; }
+			if (substr(strtolower($showDate),0,1) == 'y' or substr(strtolower($showDate),0,1) == 'c') { $commentHTML .= ' on '.date('F jS, Y',strtotime($whatsNewFreshHappening->comment_date)); $commentExtraDetail = true; }
+			if ($commentExtraDetail) { $commentSentence = '<br />'.$commentSentence; } 
+			$commentHTML .= $commentSentence.'</p>'.Chr(10);
+			
+			$whatsNewFreshHappeningHTML .= $commentHTML;
   	}
   }
   $whatsNewFreshHappeningHTML .= '</div>'.$after_widget;
   
   _e($whatsNewFreshHappeningHTML);	
 	
-  $theFile = "whatsNewFreshHappening.HTML";
-  $fh = fopen($theFile, 'w') or die("can't open file");
-  fwrite($fh, $whatsNewFreshHappeningHTML);
-  fclose($fh);
+	if (substr(strtolower($writeToFile),0,1) == 'y')
+	{
+    $theFile = "whatsNewFreshHappening.HTML";
+    $fh = fopen($theFile, 'w') or die("can't open file");
+    fwrite($fh, $whatsNewFreshHappeningHTML);
+    fclose($fh);
+	}
 ?>
